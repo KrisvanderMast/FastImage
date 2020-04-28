232de724651a33dc0a6044236aa9f925c6880560
Kris van der Mast <kris.vandermast@gmail.com>
2020-04-28 21:08:02 +0200

    Added first project

---
+++ Test/Form1.Designer.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Test
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBoxAll = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxAll)).BeginInit();
+            this.SuspendLayout();
+            //
+            // pictureBoxAll
+            //
+            this.pictureBoxAll.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBoxAll.Location = new System.Drawing.Point(0, 0);
+            this.pictureBoxAll.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.pictureBoxAll.Name = "pictureBoxAll";
+            this.pictureBoxAll.Size = new System.Drawing.Size(900, 562);
+            this.pictureBoxAll.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBoxAll.TabIndex = 0;
+            this.pictureBoxAll.TabStop = false;
+            //
+            // PX
+            //
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(900, 562);
+            this.Controls.Add(this.pictureBoxAll);
+            this.KeyPreview = true;
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Text = "Form1";
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxAll)).EndInit();
+            this.ResumeLayout(false);
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBoxAll;
+    }
+}
+
---
+++ Test/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace Test
+{
+    public partial class Form1 : Form
+    {
+        private const string PATH = @"C:\px\";
+        private Stack<string> _images; // = new Stack<string>();
+        private string _pathToCurrentImage;
+
+        public Form1()
+        {
+            InitializeComponent();
+            GetFiles();
+            CreateSubfolders();
+        }
+
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            string key = e.KeyChar.ToString();
+            var curFile = _pathToCurrentImage;
+
+            string directoryName = Path.GetDirectoryName(_pathToCurrentImage);
+            string fileName = Path.GetFileNameWithoutExtension(_pathToCurrentImage);
+            string extension = Path.GetExtension(_pathToCurrentImage);
+            string destination = Path.Combine(directoryName, key, $"{fileName}{extension}");
+
+            try
+            {
+                File.Move(curFile, destination);
+            }
+            catch (Exception)
+            {
+                destination = Path.Combine(directoryName, key, $"{fileName}_{Guid.NewGuid()}{extension}");
+                File.Move(curFile, destination);
+            }
+
+            if (_images.Count > 0)
+            {
+                ShowImage();
+            }
+        }
+
+        private void GetFiles()
+        {
+            var ext = new List<string> { ".jpg", ".gif", ".png", ".jpeg" };
+            var myFiles = Directory
+                .EnumerateFiles(PATH, "*.*", SearchOption.TopDirectoryOnly)
+                .Where(s => ext.Contains(Path.GetExtension(s).ToLowerInvariant()));
+
+            _images = new Stack<string>(myFiles);
+
+            if (_images.Count > 0)
+            {
+                ShowImage();
+            }
+        }
+
+        private void CreateSubfolders()
+        {
+            var di = new DirectoryInfo(PATH);
+
+            for (int i = 0; i < 10; i++)
+            {
+                di.CreateSubdirectory(i.ToString());
+            }
+        }
+
+        private void ShowImage()
+        {
+            _pathToCurrentImage = _images.Pop();
+
+            this.Text = $"{_images.Count} - {_pathToCurrentImage}";
+
+            Image img;
+            using (Image temp = Image.FromFile(_pathToCurrentImage))
+            {
+                img = new Bitmap(temp);
+            }
+
+            pictureBoxAll.Image = img;
+        }
+    }
+}
