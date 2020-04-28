b072adc80aaf1f056a76ceba33036ff338d5c62e
Kris van der Mast <kris.vandermast@gmail.com>
2020-04-28 20:52:36 +0200

    added solution file

---
+++ FastImage/ImageViewer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace FastImage
+{
+    public partial class ImageViewer : Form
+    {
+        private const string PATH = @"C:\px\";
+
+        public ImageViewer()
+        {
+            InitializeComponent();
+            GetFiles();
+            CreateSubfolders();
+        }
+
+        private void GetFiles()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void CreateSubfolders()
+        {
+            var di = new DirectoryInfo(PATH);
+
+            for (int i = 0; i < 10; i++)
+            {
+                di.CreateSubdirectory(i.ToString());
+            }
+        }
+    }
+}
